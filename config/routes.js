bd1db7f7ba20d78e279c42bb4fc34d6550dce630
Aleff Oliveira <aleffoliveira@192.168.1.2>
2021-10-25 00:08:16 -0300

    database working good

--- config/routes.js
+++ config/routes.js
@@ -2,7 +2,6 @@ const express = require("express");
 const routes = express.Router();
 const jobs = require("./database/Jobs");
 
-
 //Render main page
 routes.get('/', (req, res) =>{
     res.render('index');
