39b53c59c948f152bb9f0f8475dca80ad96c8025
Aleff <aleff.a.s.o@gmail.com>
2021-12-10 09:55:46 -0300

    database standard connection

--- database/database.js
+++ database/database.js
@@ -3,9 +3,9 @@ const dotenv = require('dotenv');
 
 dotenv.config({path: './.env'});
 
-const connection = new Sequelize('jobdescription', 'root', process.env.DATABASE_PASSWORD,{ //Database set up
-    host: 'localhost', // Whenever you want to run in a server you can replace localhost with the IP address
-    dialect: 'mysql'
+const connection = new Sequelize(process.env.DATABASE_TABLE, process.env.DATABASE_ROOT, process.env.DATABASE_PASSWORD,{ //Database set up
+    host: process.env.DATABASE_HOST, // Whenever you want to run in a server you can replace localhost with the IP address
+    dialect: process.env.DATABASE_DIALECT
 });
 
 module.exports = connection;
